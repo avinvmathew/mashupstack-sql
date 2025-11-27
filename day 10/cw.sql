d7e389c33b84b0155e773effc0aed8206bfc80c9
Avin Varghese Mathew <avinvmavolil@gmail.com>
2025-11-28 01:12:20 +0530

    day 10 cw and hw done

--- day 10/cw.sql
+++ day 10/cw.sql
@@ -1,11 +1,20 @@
---  You are helping design a student registration system for a training institute. You need to:
+-- You are helping design a student registration system for a training institute. You need to:
+
 -- Create a table called students with the following information:
 -- student_id (a unique number for each student)
 -- name (student's name)
 -- email (each student must have a unique email address)
+
+CREATE TABLE students( student_id int UNIQUE, name varchar(255), email varchar(255) UNIQUE );
+
 -- Create another table called courses with:
 -- course_id (a unique number for each course)
 -- course_name (name of the course)
+
+CREATE TABLE courses( course_id int UNIQUE, course_name varchar(255));
+
 -- Create a table called enrollments to store which student has enrolled in which course. This table should:
 -- Include student_id and course_id
 -- Make sure both values must match existing records in the other two tables
+
+CREATE TABLE enrollments( student_id int, course_id int, FOREIGN KEY (student_id) REFERENCES students(student_id), FOREIGN KEY (course_id) REFERENCES courses(course_id) );
--- day 10/hw.sql
+++ day 10/hw.sql
@@ -3,6 +3,9 @@
 -- Author ID (a unique number for each author)
 -- Author name (e.g., "Chetan Bhagat")
 -- Email address (should be unique)
+
+CREATE TABLE AUTHORS( author_id int UNIQUE, author_name varchar(255), email varchar(255) UNIQUE );
+
 -- Create a table for books with the following information:
 -- Book ID (a unique number for each book)
 -- Book title
@@ -11,3 +14,5 @@
 -- No two authors can have the same email address.
 -- Each author and book must have a unique ID.
 -- Each book must be linked to an existing author.
+
+CREATE TABLE books( book_id int UNIQUE, book_title varchar(255), author_id int, FOREIGN KEY (author_id) REFERENCES authors(author_id) );
